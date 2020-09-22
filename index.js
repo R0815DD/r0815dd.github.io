1801a7ed6aac768cabeef8fa2f18f980885bbdea
R0815DD <71693794+R0815DD@users.noreply.github.com>
2020-09-22 13:43:37 +0200

    change plot update rate

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ bigButton.addEventListener('click', function(event) {
         {
           constData.shift();
         }
-        if (counter>100)  // aktualisiert Plot alle 100 Werte
+        if (counter>10)  // aktualisiert Plot alle 100 Werte
         {
           counter=0;
           drawChart();

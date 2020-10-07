cf983be44969cb6795a403d2aac054ee0edbb515
R0815DD <71693794+R0815DD@users.noreply.github.com>
2020-10-07 15:12:33 +0200

    add comments

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-//Initialisere Plot mit Google Charts API
+//Initate plot with Google Charts API
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 //Label Button
 const bigButton = document.getElementById('bigButton');
+//Label Text
 const batteryLevel = document.getElementById('Batterylevel');
 
-// Initialisiere Variable
+// Initiate Variable
 let constData = new Array([0,0]);
 let counter = 0;
 let timestamp = 0;
 let volts = 0;
 
-//Callback wenn Button geklickt wird
+//Callback if Button clicked
 bigButton.addEventListener('click', function(event) {
     connectBLE();
   });
 
-  function connectBLE() //Funktion zur Vernindung mit BLE Device
+  function connectBLE() //function to connect with peripheral
   {
-    let serviceUuid = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+    let serviceUuid = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'; // define service UUID
     let characteristicUuid = '';
-      console.log("Button pressed"); //Debug Ausgabe wenn Funktion aufgerufen
-      navigator.bluetooth.requestDevice({//Filter BLE Geräte, nur Gerät mit korrekter Service UUID wird gefunden
+      navigator.bluetooth.requestDevice({//filter BLE peripheral with defined service UUID
         filters: [{
           services: [serviceUuid]
         }]
       })
-      .then(device => device.gatt.connect())
+      .then(device => device.gatt.connect()) //connect to peripheral
       //console.log("Connected");
       .then(server => {
         console.log('Getting Service...');
@@ -36,12 +36,14 @@ bigButton.addEventListener('click', function(event) {
       .then(service => {
         console.log('Getting Characteristics...');
         // Get all characteristics.
-        return service.getCharacteristics();
+        return service.getCharacteristics();  //read all device Characteristics
       })
       .then(characteristics => {
-          let batteryUuid = characteristics[1].uuid;
+        // Zuordnung UUIDs fest nach tatsächlicher UUID
+          let batteryUuid = characteristics[1].uuid;  //create objects for sensor and battery characteristic
           let sensorUuid = characteristics[0].uuid;
           
+          // setup notifications and event listerner for changed characteristic value
           batteryLevelCharacteristic = characteristics[1];
           batteryLevelCharacteristic.startNotifications();
           batteryLevelCharacteristic.addEventListener('characteristicvaluechanged',
@@ -52,46 +54,36 @@ bigButton.addEventListener('click', function(event) {
             handleSensorValueChanged);
           console.log(batteryUuid + sensorUuid);
       })
-
-
     }
- 
+  
+  function handleSensorValueChanged(event) { //fuction for changed sensor value
+    let newData = event.target.value.getUint32(0, true);  //save value in newData 
+    timestamp = newData >> 12;  //save timestamp
+    volts = newData & 0x00000FFF;//save voltage from ecg value
+    constData.push([timestamp , volts]); //save data in array
+    counter++;
+
+    if (constData.length >= 400)  // only collect last 400 values
+    {
+      constData.shift();
+    }
+    if (counter>50)  // update plot after 50 values
+    {
+      counter=0;
+      drawChart();
+    } 
+  }
     
-      
-      function handleSensorValueChanged(event) { //Funktion wenn Charakteristik Wert geändert
-        let newData = event.target.value.getUint32(0, true);  //speichert neuen Wert in newData 
-        //let millis = Date.now() - start;
-        //console.log((millis/1000) + "  " + newData);
-        timestamp = newData >> 12;  //Zeitstempel, aktuell 5 ms Zykluszeit
-        volts = newData & 0x00000FFF;
-        constData.push([timestamp , volts]); //speichert Zeitstempel und Wert in Ringspeicher
-        counter++;
-        
+  //display battery state of charge
+  function handleBatteryLevelChanged(event){
+    let batteryLevel = event.target.value.getUint8(0);
+    BatteryLevel.innerText = 'Battery Level: ' + batteryLevel + '%';
+  }
 
-        if (constData.length >= 400)  // Nur die letzten 400 Werte werden gespeichert, ältere Werte gelöscht
-        {
-          constData.shift();
-        }
-        if (counter>50)  // aktualisiert Plot alle 50 Werte
-        {
-          counter=0;
-          drawChart();
-        }
-        
-      }
-      
-      function handleBatteryLevelChanged(event){
-        let batteryLevel = event.target.value.getUint8(0);
-        BatteryLevel.innerText = 'Battery Level: ' + batteryLevel + '%';
-        // console.log('> Battery Level is ' + batteryLevel + '%');
-      }
-
-  function drawChart() {  //Funktion zeichnet Plot
+  function drawChart() {  //update plot function
     var data = new google.visualization.DataTable();
       data.addColumn('number', 'time');
-      data.addColumn('number', 'signal');
-
-      
+      data.addColumn('number', 'signal');    
       data.addRows(constData); 
       
     var options = {
@@ -99,11 +91,6 @@ bigButton.addEventListener('click', function(event) {
       curveType: 'function',
       legend: { position: 'bottom' }
     };
-
     var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    chart.draw(data, options); //Zur Optimierung, diese Zeile möglichst allein ausführen
+    chart.draw(data, options); 
   }
-
-  
-
-
